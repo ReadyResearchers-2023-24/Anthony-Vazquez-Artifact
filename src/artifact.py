8c509b9ca297093f748749bf0bae799a2cea9193
Anthony Vazquez <vazquez01@allegheny.edu>
2023-11-02 11:54:38 -0400

    Final Commit

--- src/artifact.py
+++ src/artifact.py
@@ -6,44 +6,62 @@ import random as rd
 
 DATE_COLUMN = 'date/time'
 
-@st.cache_data
-def load_data(data):
-    lowercase = lambda x: str(x).lower
-    data.rename(lowercase, axis='columns', inplace=True)
-    return data
 
 data_load_state = st.text('Loading data...')
-total_data = pd.read_csv('data/total_emissions_global_2019.csv', nrows=222)
-load_data(total_data)
-capita_data = pd.read_csv('data/emissions_per_capita_2019.csv', nrows=223)
-load_data(capita_data)
-global_data = pd.read_csv('data/emissions_global_1751_2019.csv', nrows=270)
-load_data(global_data)
-data_load_state.text("Done! (using st.cache_data)")
+total_data = pd.read_csv('data/total_emissions_global_2019.csv', dtype={"Year": str}, nrows=36)
+capita_data = pd.read_csv('data/emissions_per_capita_2019.csv', dtype={"Year": str}, nrows=223)
+global_data = pd.read_csv('data/emissions_global_1751_2019.csv', dtype={"Year": str}, nrows=270)
+data_load_state.text("Done loading data! (using st.cache_data)")
 
 if st.checkbox('Show raw data for total CO2 emissions from fossil fuels'):
-    st.subheader('Raw data(thousand metric tons)')
+    st.subheader('Raw data(thousand metric tons of C)')
     st.write(total_data)
 
-if st.checkbox('Show raw data for total global C02 emissions per capita'):
-    st.subheader('Raw data(metric tons)')
+if st.button('Show raw data for total global C02 emissions per capita'):
+    st.subheader('Raw data(metric tons of C)')
     st.write(capita_data)
 
-if st.checkbox('Show raw data for total global C02 emissions from 1751-2019'):
-    st.subheader('Raw data(million metric tons)')
+if st.toggle('Show raw data for total global C02 emissions from 1751-2019'):
+    st.subheader('Raw data(million metric tons of C)')
     st.write(global_data)
 
-st.subheader('Line Charts of global emissions from 1751 to 2019')
+st.subheader('Line chart of global emissions from 1751 to 2019.')
 
-if st.checkbox('Global Carbon Emissions by fossil fuel'):
+if st.checkbox('Global Carbon Emissions by fossil fuel(Line Chart)'):
     df = pd.read_csv('data/emissions_global_1751_2019.csv', dtype={"Year": str})
 
-    chart_data = pd.DataFrame(df, columns=["Year", "Emissions fossil fuel(million metric tons of C)", "Emissions solid fuel", "Emissions liquid fuel", "Emissions gas fuel", "Emissions cement production", "Emissions gas flaring", "Emissions per capita(metric tons of carbon)"])
+    chart_data = pd.DataFrame(df, columns=["Year", "Emissions fossil fuel", "Emissions solid fuel", "Emissions liquid fuel", "Emissions gas fuel", "Emissions cement production", "Emissions gas flaring", "Emissions per capita(metric tons of carbon)"])
 
-    st.line_chart(chart_data, x="Year", y=["Emissions fossil fuel(million metric tons of C)", "Emissions solid fuel", "Emissions liquid fuel", "Emissions gas fuel", "Emissions cement production", "Emissions gas flaring", "Emissions per capita(metric tons of carbon)"])
+    st.line_chart(chart_data, x="Year", y=["Emissions fossil fuel", "Emissions solid fuel", "Emissions liquid fuel", "Emissions gas fuel", "Emissions cement production", "Emissions gas flaring", "Emissions per capita(metric tons of carbon)"])
 
     if st.checkbox('Click to see the raw data of this graph'):
         st.write(chart_data)
+
+st.subheader('Bar chart of global emissions from 1751 to 2019.')
+
+if st.checkbox('Global Carbon Emissions by fossil fuel(Bar Chart)'):
+    df = pd.read_csv('data/emissions_global_1751_2019.csv', dtype={"Year": str})
+
+    chart_data = pd.DataFrame(df, columns=["Year", "Emissions fossil fuel", "Emissions solid fuel", "Emissions liquid fuel", "Emissions gas fuel", "Emissions cement production", "Emissions gas flaring", "Emissions per capita(metric tons of carbon)"])
+
+    st.bar_chart(chart_data, x="Year", y=["Emissions fossil fuel", "Emissions solid fuel", "Emissions liquid fuel", "Emissions gas fuel", "Emissions cement production", "Emissions gas flaring", "Emissions per capita(metric tons of carbon)"])
+
+    if st.checkbox('Click to see the raw data of this graph'):
+        st.write(chart_data)
+
+st.subheader('Area chart of global emissions from 1751 to 2019.')
+
+if st.checkbox('Global Carbon Emissions by fossil fuel(Area Chart)'):
+    df = pd.read_csv('data/emissions_global_1751_2019.csv', dtype={"Year": str})
+
+    chart_data = pd.DataFrame(df, columns=["Year", "Emissions fossil fuel", "Emissions solid fuel", "Emissions liquid fuel", "Emissions gas fuel", "Emissions cement production", "Emissions gas flaring", "Emissions per capita(metric tons of carbon)"])
+
+    st.area_chart(chart_data, x="Year", y=["Emissions fossil fuel", "Emissions solid fuel", "Emissions liquid fuel", "Emissions gas fuel", "Emissions cement production", "Emissions gas flaring", "Emissions per capita(metric tons of carbon)"])
+
+    if st.checkbox('Click to see the raw data of this graph'):
+        st.write(chart_data)
+
+st.subheader('Map of total global emissions in 2019.')
 
 if st.checkbox('Show map of global carbon emissions(thousand metric tons of C)'):
     df = pd.read_csv('data/total_emissions_global_2019.csv')
